7ac62ad1b954db343a73d7bcca980351ecda3a3d
nastaj <G00424689@atu.ie>
2023-07-03 23:05:50 +0200

    added calculator functionality

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
 "use strict";
 
+// Form for submitting the values
 const form = document.querySelector(".form");
+
+// Input fields
 const inputs = document.querySelectorAll(".input");
 const inputDay = document.querySelector(".input-day");
 const inputMonth = document.querySelector(".input-month");
 const inputYear = document.querySelector(".input-year");
-const btnSubmit = document.querySelector(".btn-submit");
+
+// DOM Elements for setting the text content
 const yearsEl = document.querySelector(".years");
 const monthsEl = document.querySelector(".months");
 const daysEl = document.querySelector(".days");
 
-const date = new Date();
-const currentYear = date.getFullYear();
-const currentMonth = date.getMonth() + 1;
-const currentDay = date.getDay();
+// Dates
+const today = new Date();
+const currentYear = today.getFullYear();
 
+// Set max value for year input to prevent setting the future year
+// Further validated in checkInputs function
 inputYear.setAttribute("max", currentYear);
 
 const renderError = function (message, target, targetError) {
+  target.parentElement.querySelector("label").style.color = "hsl(0, 100%, 67%)";
   target.style.borderColor = "hsl(0, 100%, 67%)";
   targetError.classList.remove("hidden");
   targetError.textContent = message;
@@ -25,34 +31,42 @@ const renderError = function (message, target, targetError) {
 
 const checkInputs = function () {
   let errorTarget;
+  let currentTarget;
+
   inputs.forEach((input) => {
     input.addEventListener("invalid", (e) => {
       e.preventDefault();
+
+      currentTarget = e.target;
       errorTarget = e.target.parentElement.querySelector(".error-msg");
 
       if (input.value === "") {
-        renderError("This field is required", e.target, errorTarget);
+        renderError("This field is required", currentTarget, errorTarget);
       }
 
-      if (e.target.classList.contains("input-day") && input.value > 31) {
-        renderError("Must be a valid day", e.target, errorTarget);
+      if (currentTarget.classList.contains("input-day") && input.value > 31) {
+        renderError("Must be a valid day", currentTarget, errorTarget);
       }
 
-      if (e.target.classList.contains("input-month") && input.value > 12) {
-        renderError("Must be a valid month", e.target, errorTarget);
+      if (currentTarget.classList.contains("input-month") && input.value > 12) {
+        renderError("Must be a valid month", currentTarget, errorTarget);
       }
 
       if (
-        e.target.classList.contains("input-year") &&
+        currentTarget.classList.contains("input-year") &&
         input.value > currentYear
       ) {
-        renderError("Must be in the past", e.target, errorTarget);
+        renderError("Must be in the past", currentTarget, errorTarget);
       }
     });
 
     input.addEventListener("focus", (e) => {
-      errorTarget = e.target.parentElement.querySelector(".error-msg");
-      errorTarget.classList.add("hidden");
+      currentTarget = e.target;
+
+      currentTarget.parentElement.querySelector("label").style.color = "";
+      currentTarget.parentElement
+        .querySelector(".error-msg")
+        .classList.add("hidden");
       input.style.borderColor = "";
     });
   });
@@ -62,19 +76,34 @@ checkInputs();
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  // Retrieve values passed by user
   const year = +inputYear.value;
   const month = +inputMonth.value - 1;
   const day = +inputDay.value;
 
-  const userDate = parseInt(
-    (new Date(year, month, day).getTime() / 1000).toFixed(0)
-  );
+  // Set new Date object
+  const birthDate = new Date(year, month, day);
 
-  console.log(date.valueOf() - userDate);
-  //   ^ Convert it later to ISO and store it in variable
+  // Calculate ages
+  let ageYears = today.getFullYear() - birthDate.getFullYear();
+  let ageMonths = today.getMonth() - birthDate.getMonth();
+  let ageDays = today.getDate() - birthDate.getDate();
 
-  //   Render age
-  //   yearsEl.textContent = years;
-  //   monthsEl.textContent = months;
-  //   daysEl.textContent = Math.max(days, 0);
+  // Adjust for negative ageMonths
+  if (ageMonths < 0 || (ageMonths === 0 && ageDays < 0)) {
+    ageYears--;
+    ageMonths = 12 - birthDate.getMonth() + today.getMonth() - 1;
+    ageDays = today.getDate() - birthDate.getDate();
+  }
+
+  // Adjust for negative ageDays
+  if (ageDays < 0) {
+    const lastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+    ageDays = lastMonth.getDate() - birthDate.getDate() + today.getDate();
+  }
+
+  // Render age
+  yearsEl.textContent = ageYears;
+  monthsEl.textContent = ageMonths;
+  daysEl.textContent = ageDays;
 });
